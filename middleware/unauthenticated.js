e058c2e200773d62e9747a3b2bc45ef2dfcb64de
bagussurya12 <bagus.suryaindrajati@gmail.com>
2024-06-03 17:34:55 +0700

    Update Config

--- middleware/unauthenticated.js
+++ middleware/unauthenticated.js
@@ -1,10 +1,5 @@
-export default function ({ store, i18n }) {
+export default function ({ store, redirect }) {
   if (store.getters["auth/authenticated"]) {
-    return window.$nuxt.$router.push({
-      name: "login___" + i18n.locale,
-      params: {
-        message: "AUTH_REQUIRED",
-      },
-    });
+    return redirect("/");
   }
 }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  server: {
+    port: 8000,
+  },
+  ssr: false,
+  title: "Hello Nuxt",
+  components: true,
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
   css: ["@/assets/css/tailwind.css"],
@@ -8,5 +14,8 @@ export default defineNuxtConfig({
       tailwindcss: {},
       autoprefixer: {},
     },
+  },
+  axios: {
+    baseURL: process.env.API_URL,
   },
 });
